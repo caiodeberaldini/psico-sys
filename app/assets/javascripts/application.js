11d4e0482de19ce9c6104965ab744486220f3c05
Caio Deberaldini <caio-soad@hotmail.com>
2018-11-05 17:14:48 -0200

    Login and Navbar/Sidenav view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,16 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+document.addEventListener('turbolinks:load', function() {
+    elem = document.querySelector('.sidenav');
+    instance = new M.Sidenav(elem, {});
+});
+
+document.addEventListener('turbolinks:before-visit', function() {
+    elem = document.querySelector('.sidenav');
+    instance = M.Sidenav.getInstance(elem);
+    if (instance){
+      instance.destroy();
+    }
+});
