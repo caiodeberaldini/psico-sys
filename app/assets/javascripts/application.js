fe8e2b4ab9b52556cfba33f7c925912020c14147
Caio Deberaldini <caio-soad@hotmail.com>
2018-11-12 17:04:56 -0200

    Select box include/Dynamic subjects menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,39 +1,28 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+/*require turbolinks*/
 //= require materialize-sprockets
 //= require_tree .
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: true
-    });
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: true
   });
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
-  });
-
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-  
-document.addEventListener('turbolinks:load', function() {
-    elem = document.querySelector('.sidenav');
-    instance = new M.Sidenav(elem, {});
 });
 
-document.addEventListener('turbolinks:before-visit', function() {
-    elem = document.querySelector('.sidenav');
-    instance = M.Sidenav.getInstance(elem);
-    if (instance){
-      instance.destroy();
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
 });
