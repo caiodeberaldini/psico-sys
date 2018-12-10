51f3f59f893edfd3d032ebf5482c673ded234f98
Caio Deberaldini <caio-soad@hotmail.com>
2018-12-10 14:03:27 -0200

    Text adjustment and routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -44,5 +44,7 @@ Rails.application.routes.draw do
 
   root to: "cursinho#index"
 
+  get "*path" => redirect("/")
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
