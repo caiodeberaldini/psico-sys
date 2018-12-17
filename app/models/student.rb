cfd8be0bd1f81490587eb19e9457a082c14c446a
Caio Deberaldini <caio-soad@hotmail.com>
2018-12-17 16:53:24 -0200

    Subscription details

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,20 @@
 class Student < ApplicationRecord
     has_many :users, as: :role, dependent: :destroy
+    before_save :isBlank
+
+    def isBlank
+        if self.name.blank? or self.cpf.blank? or self.cell_phone.blank? or self.address.blank? or self.city.blank? or self.state.blank? or self.scholarship.blank? or self.scholarship_need.blank? or self.situation.blank? or self.email.blank? or self.classe.blank?
+            self.name = nil
+            self.cpf = nil
+            self.cell_phone = nil
+            self.address = nil
+            self.city = nil
+            self.state = nil
+            self.scholarship = nil
+            self.scholarship_need = nil
+            self.situation = nil
+            self.email = nil
+            self.classe = nil
+        end
+    end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,17 @@
 class Teacher < ApplicationRecord
     has_many :users, as: :role, dependent: :destroy
+    before_save :isBlank
+
+    def isBlank
+        if self.name.blank? or self.cpf.blank? or self.address.blank? or self.city.blank? or self.state.blank? or self.cell_phone.blank? or self.disciplina.blank? or self.email.blank?
+           self.name = nil
+           self.cpf = nil
+           self.address = nil
+           self.city = nil
+           self.state = nil
+           self.cell_phone = nil
+           self.disciplina = nil
+           self.email = nil
+        end 
+    end
 end
