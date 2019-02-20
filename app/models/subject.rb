e9d9d9ccbf2e060676386b359ab51b935696d9a7
Caio Deberaldini <caio-soad@hotmail.com>
2019-02-20 19:58:52 -0300

    Production update v4

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,4 @@
 class Subject < ApplicationRecord
-    has_one :apostille, dependent: :delete_all
-    has_many :materials, dependent: :delete_all
+    has_one :apostille, dependent: :delete
+    has_many :materials, dependent: :delete
 end
